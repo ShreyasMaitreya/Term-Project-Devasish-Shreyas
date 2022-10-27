03f81c9802b6150688434c363c5288b8dc8c8dc8
ShreyasMaitreya <shreyasmaitreya34@gmail.com>
2022-10-27 02:57:35 -0500

    pi_exec_time_parallel.c

--- pi_exec_time.c
+++ pi_exec_time.c
@@ -1,8 +1,9 @@
-﻿//calculating pi upto a specific number of digits can give an insight into both the clock rate and the floating point performance of the given CPU
+//calculating pi upto a specific number of digits can give an insight into both the clock rate and the floating point performance of the given CPU
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include<time.h>
+#include<pthread.h>
 
 //calculating pi using lebniz expansion
 
@@ -27,20 +28,26 @@ int pi_calc()
     return 0;
 }
 
-int main()
+int benchmark()
 
 {
     clock_t t;
     float pi;
     t = clock();
     pi = pi_calc();
-    printf("%f \n pi is calculated to in ", pi);
+    printf("%f \n pi is calculated in ", pi);
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
     //number of clock cycles = clocks per second*execution time
-    double cycles = time_taken*CLOCKS_PER_SEC;
+    double cycles = time_taken*CLOCKS_PER_SEC/1000; //in windows, CLOCKS_PER_SEC returns time in milliseconds
     printf("%f seconds\n", time_taken);
     printf("%f cycles\n", cycles);
 
+    return 0;
+}
+
+int main()
+{
+    benchmark();
     return 0;
 }
